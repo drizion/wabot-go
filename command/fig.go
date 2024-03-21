1ed13b3d6c88da36fae90c252306e2a616ab7e2e
Gabriel da Silva <gabrieldasilvadevargas@gmail.com>
2024-03-21 10:03:27 -0300

    move WebpWriteExifData to helpers

--- command/fig.go
+++ command/fig.go
@@ -1,21 +1,14 @@
 package command
 
 import (
-	"bytes"
 	"context"
-	"encoding/binary"
-	"encoding/json"
 	"fmt"
 	"os"
-	"os/exec"
-	"path"
-	"strconv"
 	"time"
 
 	c "github.com/drizion/wabot-go/client"
 	"github.com/drizion/wabot-go/helpers"
 	"github.com/h2non/bimg"
-	"go.mau.fi/libsignal/logger"
 	"go.mau.fi/whatsmeow"
 	waProto "go.mau.fi/whatsmeow/binary/proto"
 	"go.mau.fi/whatsmeow/types/events"
@@ -53,7 +46,7 @@ func Fig(msg *events.Message) {
 		return
 	}
 
-	stickerBytes, err = WebpWriteExifData(newImage, msg.Info.Timestamp.Unix())
+	stickerBytes, err = helpers.WebpWriteExifData(newImage, msg.Info.Timestamp.Unix())
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 	}
@@ -87,73 +80,4 @@ func Fig(msg *events.Message) {
 	}
 
 	fmt.Println("Message sent:", res)
-}
-
-func WebpWriteExifData(inputData []byte, updateId int64) ([]byte, error) {
-	var (
-		startingBytes = []byte{0x49, 0x49, 0x2A, 0x00, 0x08, 0x00, 0x00, 0x00, 0x01, 0x00, 0x41, 0x57, 0x07, 0x00}
-		endingBytes   = []byte{0x16, 0x00, 0x00, 0x00}
-		b             bytes.Buffer
-
-		currUpdateId = strconv.FormatInt(updateId, 10)
-		currPath     = path.Join("downloads", currUpdateId)
-		inputPath    = path.Join(currPath, "input_exif.webm")
-		outputPath   = path.Join(currPath, "output_exif.webp")
-		exifDataPath = path.Join(currPath, "raw.exif")
-	)
-
-	if _, err := b.Write(startingBytes); err != nil {
-		return nil, err
-	}
-
-	jsonData := map[string]interface{}{
-		"sticker-pack-id":        "drizion.dev",
-		"sticker-pack-name":      "wabot.net 🤖",
-		"sticker-pack-publisher": "@eu_drizion (segue aí rs)",
-		"emojis":                 []string{"😀"},
-	}
-	jsonBytes, err := json.Marshal(jsonData)
-	if err != nil {
-		return nil, err
-	}
-
-	jsonLength := (uint32)(len(jsonBytes))
-	lenBuffer := make([]byte, 4)
-	binary.LittleEndian.PutUint32(lenBuffer, jsonLength)
-
-	if _, err := b.Write(lenBuffer); err != nil {
-		return nil, err
-	}
-	if _, err := b.Write(endingBytes); err != nil {
-		return nil, err
-	}
-	if _, err := b.Write(jsonBytes); err != nil {
-		return nil, err
-	}
-
-	if err := os.MkdirAll(currPath, os.ModePerm); err != nil {
-		return nil, err
-	}
-	defer os.RemoveAll(currPath)
-
-	if err := os.WriteFile(inputPath, inputData, os.ModePerm); err != nil {
-		return nil, err
-	}
-	if err := os.WriteFile(exifDataPath, b.Bytes(), os.ModePerm); err != nil {
-		return nil, err
-	}
-
-	cmd := exec.Command("webpmux",
-		"-set", "exif",
-		exifDataPath, inputPath,
-		"-o", outputPath,
-	)
-
-	if err := cmd.Run(); err != nil {
-		logger.Debug("failed to run webpmux command")
-
-		return nil, err
-	}
-
-	return os.ReadFile(outputPath)
 }
--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,15 +1,33 @@
 package helpers
 
 import (
+	"bytes"
 	"context"
+	"encoding/binary"
+	"encoding/json"
 	"fmt"
+	"os"
+	"os/exec"
+	"path"
+	"strconv"
 	"time"
 
 	c "github.com/drizion/wabot-go/client"
+	"go.mau.fi/libsignal/logger"
 	"go.mau.fi/whatsmeow"
 	waProto "go.mau.fi/whatsmeow/binary/proto"
 	"go.mau.fi/whatsmeow/types/events"
 	"google.golang.org/protobuf/proto"
+)
+
+const (
+	ErrorReaction   string = "❌"
+	SuccessReaction string = "✅"
+	LoadingReaction string = "⏳"
+	PingReaction    string = "🏓"
+	LoveReaction    string = "❤️"
+	LikeReaction    string = "👍"
+	DislikeReaction string = "👎"
 )
 
 func Reply(m *events.Message, text string) {
@@ -61,12 +79,71 @@ func SendReact(m *events.Message, reaction string) whatsmeow.SendResponse {
 	return resp
 }
 
-const (
-	ErrorReaction   string = "❌"
-	SuccessReaction string = "✅"
-	LoadingReaction string = "⏳"
-	PingReaction    string = "🏓"
-	LoveReaction    string = "❤️"
-	LikeReaction    string = "👍"
-	DislikeReaction string = "👎"
-)
+func WebpWriteExifData(inputData []byte, updateId int64) ([]byte, error) {
+	var (
+		startingBytes = []byte{0x49, 0x49, 0x2A, 0x00, 0x08, 0x00, 0x00, 0x00, 0x01, 0x00, 0x41, 0x57, 0x07, 0x00}
+		endingBytes   = []byte{0x16, 0x00, 0x00, 0x00}
+		b             bytes.Buffer
+
+		currUpdateId = strconv.FormatInt(updateId, 10)
+		currPath     = path.Join("downloads", currUpdateId)
+		inputPath    = path.Join(currPath, "input_exif.webm")
+		outputPath   = path.Join(currPath, "output_exif.webp")
+		exifDataPath = path.Join(currPath, "raw.exif")
+	)
+
+	if _, err := b.Write(startingBytes); err != nil {
+		return nil, err
+	}
+
+	jsonData := map[string]interface{}{
+		"sticker-pack-id":        "drizion.dev",
+		"sticker-pack-name":      "wabot.net 🤖",
+		"sticker-pack-publisher": "@eu_drizion (segue aí rs)",
+		"emojis":                 []string{"😀"},
+	}
+	jsonBytes, err := json.Marshal(jsonData)
+	if err != nil {
+		return nil, err
+	}
+
+	jsonLength := (uint32)(len(jsonBytes))
+	lenBuffer := make([]byte, 4)
+	binary.LittleEndian.PutUint32(lenBuffer, jsonLength)
+
+	if _, err := b.Write(lenBuffer); err != nil {
+		return nil, err
+	}
+	if _, err := b.Write(endingBytes); err != nil {
+		return nil, err
+	}
+	if _, err := b.Write(jsonBytes); err != nil {
+		return nil, err
+	}
+
+	if err := os.MkdirAll(currPath, os.ModePerm); err != nil {
+		return nil, err
+	}
+	defer os.RemoveAll(currPath)
+
+	if err := os.WriteFile(inputPath, inputData, os.ModePerm); err != nil {
+		return nil, err
+	}
+	if err := os.WriteFile(exifDataPath, b.Bytes(), os.ModePerm); err != nil {
+		return nil, err
+	}
+
+	cmd := exec.Command("webpmux",
+		"-set", "exif",
+		exifDataPath, inputPath,
+		"-o", outputPath,
+	)
+
+	if err := cmd.Run(); err != nil {
+		logger.Debug("failed to run webpmux command")
+
+		return nil, err
+	}
+
+	return os.ReadFile(outputPath)
+}

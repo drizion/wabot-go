3a4d5436421db6af518bcbdec3e76a0e3781e998
Gabriel da Silva <gabrieldasilvadevargas@gmail.com>
2024-03-22 00:25:10 -0300

    fix command trigger

--- handler/message.go
+++ handler/message.go
@@ -2,25 +2,32 @@ package handler
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/drizion/wabot-go/command"
+	"github.com/drizion/wabot-go/config"
+	"github.com/drizion/wabot-go/helpers"
 	"go.mau.fi/whatsmeow/types/events"
 )
 
 func MessageHandler(m *events.Message) {
 	text := GetTextFromMessage(m)
-	fmt.Println("Received a message!", text, m.Info)
 
-	if text != "" {
-		cmd, exists := command.Registry.GetCommand(text)
-		if exists {
-			fmt.Println("Command found:", cmd.Triggers)
-			cmd.Exec(m)
-			return
-		}
-		fmt.Println("Command not found:", text)
+	if m.Info.IsFromMe || text == "" || text[0] != config.Prefix[0] {
+		return
 	}
 
+	fmt.Println("Received a message!", text)
+
+	text = text[1:]
+	trigger := strings.Split(strings.TrimSpace(text), " ")[0]
+
+	cmd, exists := command.Registry.GetCommand(trigger)
+	if exists {
+		cmd.Exec(m)
+		return
+	}
+	helpers.Reply(m, "Comando não encontrado!\nUse `"+config.Prefix+"menu` para ver a lista de comandos.")
 }
 
 func GetTextFromMessage(m *events.Message) string {
